f95affd8ffa7b602a68b81d843c95052c70f4f7b
stannyweb <stannyweb@gmail.com>
2025-03-02 01:57:20 +0200

    Demonstrate JUnit annotations in automation testing
    - Added a test class with @BeforeAll, @AfterAll, and @Test.

---
+++ src/test/java/taudemo/SeleniumTest.java
@@ -0,0 +1,23 @@
+package taudemo;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SeleniumTest {
+
+    @BeforeAll
+    public static void beforeAll() {
+
+    }
+
+    @AfterAll
+    public static void afterAll() {
+
+    }
+
+    @Test
+    public void openPageAndCheckTitle() {
+
+    }
+}
