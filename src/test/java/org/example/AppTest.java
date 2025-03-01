cc609b5a408d8646c5a0c10c43dd26c74f24faf9
stannyweb <stannyweb@gmail.com>
2025-03-01 19:09:28 +0200

    Initial commit

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -7,11 +7,11 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
     extends TestCase
 {
     /**
-     * Create the test case
+     *
      *
      * @param testName name of the test case
      */
