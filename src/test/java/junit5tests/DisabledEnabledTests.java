ae61def44a8f4177da9516dc291395cba9c80e82
stannyweb <stannyweb@gmail.com>
2025-03-03 21:08:39 +0200

    Add JUnit 5 example: Conditional Test Disabling
    - Created a class to demonstrate @Disabled, @DisabledIfSystemProperty, @DisabledIf, and @DisabledOnOS.
    - Illustrated disabling tests based on conditions and OS.

---
+++ src/test/java/junit5tests/DisabledEnabledTests.java
@@ -0,0 +1,53 @@
+package junit5tests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIf;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+public class DisabledEnabledTests {
+
+    @DisabledOnOs(value = OS.WINDOWS, disabledReason = "Disabled for demo of @DisabledOnOS")
+    @Test
+    void firstTest() {
+//        Тестът се деактивира, ако се изпълнява на операционна система Windows.
+        System.out.println("This is the first test");
+    }
+
+    @DisabledIfSystemProperty(named = "env", matches = "staging", disabledReason = "Disabled for demo of @disabledIfSystemProperty")
+    @Test
+    void secondTest() {
+//        Тестът се деактивира, ако системното свойство "env" е зададено на "staging".
+        System.out.println("This is the second test");
+    }
+
+    @DisabledIf(value = "provider", disabledReason = "Disabled for demo of @DisabledIf")
+    @Test
+    void thirdTest() {
+//        Тестът се деактивира, ако методът "provider()" връща true.
+        System.out.println("This is the third test");
+    }
+
+    @Test
+    void fourthTest() {
+//        Този тест винаги се изпълнява, без условия за деактивиране.
+        System.out.println("This is the fourth test");
+    }
+
+    @Disabled(value = "Disabled for demo of @Disabled")
+    @Test
+    void fifthTest() {
+//        Този тест винаги е деактивиран, независимо от условията.
+        System.out.println("This is the fifth test");
+    }
+
+    boolean provider() {
+//        Методът връща true, ако текущият ден е понеделник.
+        return LocalDateTime.now().getDayOfWeek().equals(DayOfWeek.MONDAY);
+    }
+}
